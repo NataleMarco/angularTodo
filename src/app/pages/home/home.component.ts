123a5f4e6e8887b9a43b57fafcd786a7641754ef
MarcosNatale <marcosjaviernatale@gmail.com>
2024-11-06 13:25:46 +0000

    terminado el todo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
-import {Component, computed, effect, Injector,inject , signal} from '@angular/core';
+import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
-import {Task} from '../../../models/task.model';
+import { Task } from '../../../models/task.model';
 
 
 @Component({
@@ -31,7 +31,7 @@ export class HomeComponent {
   }
 
 
-  injector = inject(Injector);
+  injector = inject(Injector)
 
   trackTasks(){
     effect(() =>{
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,13 +1,13 @@
-import {Component, signal} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   welcome = 'Hola!';
@@ -17,38 +17,45 @@ export class LabsComponent {
     'Empezar a escribir como tontito',
     'Crear componentes',
     'Crear paginas',
-    'Seguir escribiendo como tontito'
+    'Seguir escribiendo como tontito',
   ]);
 
-  disabled= 'True';
+  disabled = 'True';
 
   colorCtrl = new FormControl();
 
-  nameCtrl = new FormControl('hola',{
+  nameCtrl = new FormControl('hola', {
     nonNullable: true,
-    validators: [
-      Validators.required,
-      Validators.minLength(3)
-    ]
-    });
+    validators: [Validators.required, Validators.minLength(3)],
+  });
 
-  name = signal('Marcos')
-  person = {
-    name: 'Marcos',
+  name = signal('Marcos');
+
+  persona = signal({
+    name: 'valentina',
     age: 21,
-    avatar: 'https://w3schools.com/howto/img_avatar.png'
-  }
-
-
+    avatar: 'https://w3schools.com/howto/img_avatar.png',
+  });
 
   clickHandler() {
-    alert('Hola')
+    alert('Hola');
   }
-  changeHandler(event: Event){
-    console.log(event)
+  changeHandler(event: Event) {
+    console.log(event);
+  }
 
+  newNombreCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+
+  nameChanger(){
+    const newValue = this.newNombreCtrl.value.trim()
+    this.name.set(newValue)
   }
-  keydownHandler(event: KeyboardEvent){
+
+
+  keydownHandler(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
     this.name.set(newValue);
